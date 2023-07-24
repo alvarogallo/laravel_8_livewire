3d82c455b86b7e91cb540f14fd44f612056ea756
alvaro gallo <alvarogallo@hotmail.com>
2023-07-24 10:57:58 -0400

    Eliminando todos los commits

---
+++ app/Http/Livewire/PrimerComponente.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class PrimerComponente extends Component
+{
+    public function render()
+    {
+        return view('livewire.primer-componente');
+    }
+}
